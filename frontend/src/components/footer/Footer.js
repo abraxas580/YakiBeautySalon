1cb41d91b1ab277b188938eb193b95df8dd416e6
EnriqueHuezo <00237121@uca.edu.sv>
2023-05-02 05:32:44 -0500

    Footer component completed

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -1,53 +1,41 @@
 import React from 'react'
 import './footer.css'
-import { FaBeer } from 'react-icons/fa';
+import { RxScissors } from "react-icons/rx";
+import { FaFacebookSquare, FaTwitterSquare, FaWhatsappSquare } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai"
 
 const Footer = () => {
      return (
           <footer>
-               <div className='container-icons'>
-                    <ul>
-                         <li><FaBeer/></li>
-                         <li></li>
-                         <li></li>
-                         <li></li>
-                    </ul>
-               </div>
-               
-               <div className='container-information'>
-                    <div className='about'>
-                         <ul>
+               <div className='container-footer'>
+                    <hr className='line-separation'/>
+                    
+                    <div className='container-information'>
+                         <div className='about'>
+                              <li><RxScissors className='icon'/></li>
                               <p>
-                                   Texto de referencia para indicar que existe un texto
-                                   mamalon para describir que sucede en esta wea
+                                   Texto de referencia para indicar que existe un texto mamalon para describir que sucede en esta wea
                               </p>
-                         </ul>
-                    </div>
+                         </div>
 
-                    <div className='contacts'>
-                         <ul>
-                              <li>
-                                   Pais
-                              </li>
+                         <div className='contacts'>
+                              <ul>
+                                   <li><RxScissors className='icon'/></li>
+                                   <li>El salvador</li>
+                                   <li>Avenida los proceres, calle libertads</li>
+                                   <li>503+ 1234-5678</li>
+                                   <li>belleza@gmail.com</li>
+                              </ul>
+                         </div>
 
-                              <li>
-                                   Ubicacion
-                              </li>
-
-                              <li>
-                                   Telefono
-                              </li>
-
-                              <li>
-                                   Correo electronico
-                              </li>
-                         </ul>
-                    </div>
-
-                    <div className='SocialMedia'>
-                         <ul>
-
-                         </ul>
+                         <div className='social-media'>
+                              <ul className='container-icons-media'>
+                                   <li><FaFacebookSquare className='icon'/></li>
+                                   <li><AiFillInstagram className='icon'/></li>
+                                   <li><FaTwitterSquare className='icon'/></li>
+                                   <li><FaWhatsappSquare className='icon'/></li>
+                              </ul>
+                         </div>
                     </div>
                </div>
           </footer>
--- frontend/src/views/home/Home.js
+++ frontend/src/views/home/Home.js
@@ -3,8 +3,7 @@ import React from 'react'
 const Home = () => {
      return (
                <div>
-                    <h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3><h3>Home</h3>
-                    
+                    <h1>Texto</h1>
                </div>
      )
 }
